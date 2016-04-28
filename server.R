4924711f06c5d6aa55147437ae8cc40b1a28e4c4
Caleb Lareau <lareauc@Calebs-MacBook-Pro.local>
2016-04-28 16:05:23 -0400

    Gene/Region plotting

--- server.R
+++ server.R
@@ -6,14 +6,29 @@ library(diffloop)
 library(Sushi)
 
 function(input, output) {
-
+  val <- reactiveValues(region = NULL)
+  
   rda<-paste(system.file('rda',package='diffloop'),'loops.small.rda',sep='/')
   load(rda)
-    
-  region <- eventReactive(input$plot.region, {
-     GRanges(seqnames=c(input$chr),ranges=IRanges(start=c(as.numeric(input$start)),end=c(as.numeric(input$stop))))
+
+  observeEvent(input$plot.region, {
+     val$region <- GRanges(seqnames=c(input$chr),ranges=IRanges(start=c(as.numeric(input$start)),end=c(as.numeric(input$stop))))
   })
   
-  output$plot <- renderPlot({loopPlot(loops.small, region())}, height= 700)
-    
+  observeEvent(input$plot.gene, {
+        rda<-paste(system.file('rda',package='diffloop'),'human.genes.rda',sep='/')
+        load(rda)
+        t <- human.genes[mcols(human.genes)$id == as.character(input$Gene) ]
+        val$region <- padGRanges(t, pad = as.integer(width(t)/2))
+    })  
+  
+  observeEvent(input$clear, {
+     val$region <- NULL
+  })
+
+  
+  output$plot <- renderPlot({
+    if (is.null(val$region)) return()
+    loopPlot(loops.small, val$region)
+      }, height= 700)
  }
--- ui.R
+++ ui.R
@@ -8,12 +8,15 @@ library(Sushi)
 pageWithSidebar(
   headerPanel("DNA Regional Viewer"),
   sidebarPanel(
+    checkboxInput("np1", "Naive_Rep1_SMC1-ChIA-PET", TRUE),
     textInput("chr", "Chromsome", value = "1", width = NULL, placeholder = NULL),
     textInput("start", "Start", value = "36000000", width = NULL, placeholder = NULL),
     textInput("stop", "Stop", value = "36100000", width = NULL, placeholder = NULL),
-    textInput("Gene", "Gene", value = "MYC", width = NULL, placeholder = NULL),
+    textInput("Gene", "Gene", value = "AGO3", width = NULL, placeholder = NULL),
     actionButton("plot.region", "Plot Region"), 
-    actionButton("plot.gene", "Plot Gene")
+    actionButton("plot.gene", "Plot Gene"),
+    actionButton("clear", "Clear")
+
   ),
 
   mainPanel(plotOutput("plot"))
