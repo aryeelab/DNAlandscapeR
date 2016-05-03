abf322f75edfca7787b482040b5f06748ac8b3a9
Caleb Lareau <lareauc@Calebs-MacBook-Pro.local>
2016-05-02 22:54:51 -0400

    Fixed funciton to save the plot

--- server.R
+++ server.R
@@ -15,35 +15,51 @@ function(input, output, session) {
     observeEvent(input$plot.region, {
         val$region <- GRanges(seqnames=c(input$chr),ranges=IRanges(start=c(as.numeric(input$start)),end=c(as.numeric(input$stop))))
         })
+    
+    updateRegionVals <- function(){
+        updateNumericInput(session, "chr", value = as.numeric(seqnames(val$region)))       
+        updateNumericInput(session, "start", value = as.integer(start(ranges(range(val$region)))))  
+        updateNumericInput(session, "stop", value = as.integer(end(ranges(range(val$region)))))     
+    }
   
     observeEvent(input$plot.gene, {
         rda<-paste(system.file('rda',package='diffloop'),'human.genes.rda',sep='/')
         load(rda)
         t <- human.genes[mcols(human.genes)$id == as.character(input$Gene) ]
         val$region <- padGRanges(t, pad = as.integer(width(t)/2))
-        updateNumericInput(session, "chr", value = as.numeric(seqnames(val$region)))       
-        updateNumericInput(session, "start", value = as.integer(start(ranges(range(val$region)))))  
-        updateNumericInput(session, "stop", value = as.integer(end(ranges(range(val$region)))))       
-
+        updateRegionVals()
     })  
   
     observeEvent(input$zoom.out, {
+        if (is.null(val$region)) return()
         val$region <- padGRanges(val$region, pad = as.integer(width(val$region)/2))
+        updateRegionVals()
     })
     
      observeEvent(input$zoom.in, {
+        if (is.null(val$region)) return()
         val$region <- padGRanges(val$region, pad = -1*as.integer(width(val$region)/4))
+        updateRegionVals()
     })
      
       observeEvent(input$clear, {
         val$region <- NULL
     })
-
-
-    output$plot <- renderPlot({
+      
+    observeEvent(input$down, {
+        output$plotsave <- downloadHandler(
+            filename = paste('plot-', Sys.Date(), '.pdf', sep=''),
+            content = function(file){
+                pdf(file = file, width=8.5, height=11)
+                output$plot
+                dev.off()
+            })
+    })
+    
+    p1 <- function(){  
         if (is.null(val$region)) return()
         if (length(input$tracks) == 0) return()
-        par(mfrow=c(length(input$tracks)+input$showgenes, 1), oma = c(0, 0, 0, 0), mar = c(3, 5, 1, 1))
+        par(mfrow=c(length(input$tracks)+input$showgenes, 1), oma = c(0, 0, 1, 0), mar = c(3, 5, 1, 1))
         for(i in input$tracks){
             i <- as.integer(i)
             if (i < 1000){ #ChIA-PET from Data Source File
@@ -55,5 +71,17 @@ function(input, output, session) {
             } else {return()}
         }
         if(input$showgenes) humanAnnotation(val$region)
+}
+    output$down <- downloadHandler(
+        filename <- function() {
+            paste('plot-', Sys.Date(), '.pdf', sep='') },
+        content <- function(file) {
+            pdf(file, width = 8.5, height = 11)
+            p1()
+            dev.off()}
+    )
+      
+    output$plot <- renderPlot({
+        p1()
      }, height = 700)
  }
--- ui.R
+++ ui.R
@@ -2,14 +2,14 @@ library(shiny)
 source("global.R")
 
 pageWithSidebar(
-  headerPanel("DNA Regional Viewer"),
+  headerPanel("DNAlandscapeR"),
   sidebarPanel(
     selectInput("tracks", label = h3("Tracks"), 
         choices = f.list, 
         selectize = TRUE, multiple= TRUE, selected = 0),
-    textInput("chr", "Chromsome", value = "1", width = NULL, placeholder = NULL),
-    textInput("start", "Start", value = "36000000", width = NULL, placeholder = NULL),
-    textInput("stop", "Stop", value = "36300000", width = NULL, placeholder = NULL),
+    textInput("chr", "Chromsome", value = "12", width = NULL, placeholder = NULL),
+    textInput("start", "Start", value = "12660203", width = NULL, placeholder = NULL),
+    textInput("stop", "Stop", value = "12777226", width = NULL, placeholder = NULL),
     textInput("Gene", "Gene", value = "AGO3", width = NULL, placeholder = NULL),
     checkboxInput("showgenes", "Show Gene Annotation", value = TRUE, width = NULL),
     actionButton("plot.region", "Plot Region"), 
