b473367dfa649b1f0e0970b7341dfc595aedb08a
Caleb Lareau <lareauc@Calebs-MacBook-Pro.local>
2016-06-13 22:45:09 -0400

    Increased max file size

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ source("plotter.R")
 source("global.R")
 
 options(shiny.error=browser)
-options(shiny.maxRequestSize=250*1024^2) #250 MB Max file size; can reparameterize if needed
+options(shiny.maxRequestSize=10*1024^3) #10 GB Max file size
 options(warn=-1)
 
 function(input, output, session) {
